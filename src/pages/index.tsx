e4221ce8e4a98a7b2b3ccfcdbd4897dec5702876
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-02-12 22:49:27 -0700

    getting skeleton of the front end up and going

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,63 +1,33 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-
-import Counter from '../features/counter/Counter'
+import type { NextPage, GetStaticProps } from 'next'
 import styles from '../styles/Home.module.css'
 
-const IndexPage: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Redux Toolkit</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <header className={styles.header}>
-        <img src="/logo.svg" className={styles.logo} alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className={styles.link}
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className={styles.link}
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className={styles.link}
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className={styles.link}
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  )
+const IndexPage: NextPage = (props) => {
+    return (
+        <div className={styles.container}>
+            <h1>This is the workout for today</h1>
+            <h3>Legs Heavy</h3>
+            <ul className='my-4'>
+                <li>
+                    <strong>Squat</strong>
+                    <h5 className='mb-4'>5 Sets 5, 5, 5, 5, 5 </h5>
+                    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 '>
+                        <input type="number" className='outline mx-2 mb-4 lg:mb-0' />
+                        <input type="number" className='outline  mx-2 mx-2 mb-4 lg:mb-0' />
+                        <input type="number" className='outline  mx-2 mx-2 mb-4 lg:mb-0' />
+                        <input type="number" className='outline  mx-2 mx-2 mb-4 lg:mb-0' />
+                        <input type="number" className='outline  mx-2 mx-2 mb-4 lg:mb-0' />
+                    </div>
+
+                </li>
+            </ul>
+            <button className='px-5 rounded-full bg-blue-700 mx-1 text-white hover:bg-white hover:text-blue-700 hover:outline'>Save</button>
+            <button className='px-5 rounded-full bg-green-700 mx-1 text-white  hover:bg-white hover:text-green-700 hover:outline'>Submit</button>
+        </div >
+    )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    return { props: { a: true } }
 }
 
 export default IndexPage
