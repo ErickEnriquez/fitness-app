c51243e21d2379ab8e0c4f2eb8cbc2838770d475
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-02-14 23:35:39 -0700

    working on showing workouts on the front end

---
+++ src/server/getWorkoutTemplate.ts
@@ -0,0 +1,7 @@
+import prisma from 'prisma/prisma';
+import {Workout} from '@prisma/client';
+//return all of the possible workout types , ie push heavy, pull heavy, legs light
+export async function getWorkoutTemplate():Promise<Workout[]> {
+	const workoutArray = await prisma.workout.findMany({})
+	return workoutArray
+}
