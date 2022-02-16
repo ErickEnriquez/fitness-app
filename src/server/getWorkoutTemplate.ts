4f1c49a672855e03342c19082fbd289387c04997
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-02-16 15:35:22 -0700

    linting changes

--- src/server/getWorkoutTemplate.ts
+++ src/server/getWorkoutTemplate.ts
@@ -1,5 +1,5 @@
-import prisma from 'prisma/prisma';
-import {Workout} from '@prisma/client';
+import prisma from 'prisma/prisma'
+import {Workout} from '@prisma/client'
 //return all of the possible workout types , ie push heavy, pull heavy, legs light
 export async function getWorkoutTemplate():Promise<Workout[]> {
 	const workoutArray = await prisma.workout.findMany({})
