9ea471093ccf89a5638ecd9eec9029708de2922a
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-02-17 14:58:16 -0700

    update the index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@
 import type { NextPage, GetStaticProps } from 'next'
 import styles from '../styles/Home.module.css'
 import { getWorkoutTemplate } from '@server/getWorkoutTemplate'
-import { Workout, ExerciseTemplate } from '@prisma/client'
+import { Workout, ExerciseTemplate, ExerciseEntry } from '@prisma/client'
 import React, { useState } from 'react'
 import { useUpdateEffect } from '@hooks/useUpdateEffect'
 import axios from 'axios'
@@ -13,6 +13,9 @@ interface props {
 
 interface WorkoutTemplate extends ExerciseTemplate {
 	name: string
+	weights: number[]
+	notes: string
+	intensity?: number
 }
 
 const IndexPage: NextPage = (props: props) => {
@@ -21,9 +24,11 @@ const IndexPage: NextPage = (props: props) => {
 
 
 	useUpdateEffect(async () => {
+
 		const exercisesList = await axios.get('/api/getExercises', { params: { workoutId: workout.id } })
-		console.log(exercisesList.data)
-		setExercises([...exercisesList.data])
+		const data = exercisesList.data
+		setExercises([...data])
+
 	}, [workout])
 
 	const workoutOptions = props.workouts.map((item, idx) => {
@@ -33,16 +38,14 @@ const IndexPage: NextPage = (props: props) => {
 	})
 
 	const exercisesToDo = exercises.map((item, idx) => {
+		const inputs = Array(item.sets).fill(0)
 		return (
 			<li key={idx}>
 				<strong>{item.name}</strong>
 				<h5 className='mb-4'>Sets {item.sets}x{item.reps}</h5>
-				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5">
-					<input type="number" className='outline mx-2 mb-4 lg:mb-0' />
-					<input type="number" className='outline  mx-2 mx-2 mb-4 lg:mb-0' />
-					<input type="number" className='outline  mx-2 mx-2 mb-4 lg:mb-0' />
-					<input type="number" className='outline  mx-2 mx-2 mb-4 lg:mb-0' />
-					<input type="number" className='outline  mx-2 mx-2 mb-4 lg:mb-0' />
+
+				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 content-center">
+					{inputs.map((item, i) => <input key={i} type="number" value={item} className='outline mx-2 mb-4 lg:mb-0' />)}
 				</div>
 			</li>
 		)
@@ -51,15 +54,15 @@ const IndexPage: NextPage = (props: props) => {
 	return (
 		<div className={styles.container}>
 			<h1>This is the workout for today</h1>
-			<ul className='my-4'>
-				{exercisesToDo}
-			</ul>
-
 			<select name="" id=""
 				onChange={(e) => setWorkout(props.workouts.find(item => item.type === e.target.value))}
 			>
+				<option hidden disabled selected>-- select an option --</option>
 				{workoutOptions}
 			</select>
+			<ul className='my-4'>
+				{exercisesToDo}
+			</ul>
 			<button className='px-5 rounded-full bg-blue-700 mx-1 text-white hover:bg-white hover:text-blue-700 hover:outline'>Save</button>
 			<button className='px-5 rounded-full bg-green-700 mx-1 text-white  hover:bg-white hover:text-green-700 hover:outline'>Submit</button>
 		</div >
