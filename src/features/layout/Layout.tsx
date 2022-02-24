14698ac77bce6b6d945ef9146a48b1b092d95b74
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-02-24 11:59:44 -0700

    create layout component to wrap pages

---
+++ src/features/layout/Layout.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Head from 'next/head'
+import Iamge from 'next/image'
+
+interface LayoutProps {
+	title?: string
+	children?: React.ReactNode
+
+}
+
+const Layout = ({ children, title }: LayoutProps) => {
+	const siteTitle = title || 'Exercise Tracker'
+	return (
+		<>
+			<Head>
+				<title>{siteTitle}</title>
+				<meta name={siteTitle}></meta>
+				{/* Include the favicon when we have one */}
+				{/* <link rel="icon" href="" /> */}
+			</Head>
+			<div className='bg-slate-800 h-full'>
+				{children}
+			</div>
+		</>
+	)
+}
+
+export default Layout
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { NextPage } from 'next'
 import { useAppDispatch, useAppSelector } from '@app/hooks'
 import Loading from '@features/loading/Loading'
 import ExerciseList from '@features/exercise/ExerciseList'
+import Layout from '@features/layout/Layout'
 import {
 	//actions
 	clearEntries,
@@ -43,37 +44,38 @@ const IndexPage: NextPage = () => {
 
 	if (status === 'loading') return <Loading />
 	return (
-		<div className='text-center bg-slate-800 text-white'>
-			<h1 className='text-white'>Exercise to do</h1>
-			<select
-				className="text-slate-700 px-4 rounded-full outline outline-green-700 outline-4 mb-4"
-				onChange={(e) => dispatch(getExerciseAsync(Number(e.target.value)))}
-			>
-				<option hidden disabled selected>-- select an option --</option>
-				{workoutOptions}
-			</select>
-			<ul>
-				<ExerciseList />
-			</ul>
-			<div className='my-4'>
-				<button
-					className='px-5 rounded-full bg-green-700 mx-1 text-white  hover:bg-white hover:text-green-700 hover:outline'
-					onClick={() => dispatch(postExerciseEntries())}
+		<Layout>
+			<main className='text-center'>
+				<h1 className='text-white'>Exercise to do</h1>
+				<select
+					className="text-slate-700 px-4 rounded-full outline outline-green-700 outline-4 mb-4"
+					onChange={(e) => dispatch(getExerciseAsync(Number(e.target.value)))}
 				>
-					Submit
-				</button>
-				<button
-					className='px-5 rounded-full bg-red-700 mx-1 text-white  hover:bg-white hover:text-red-700 hover:outline'
-					onClick={() => {
-						const a = confirm('Are you sure you want to clear the workout?')
-						if (a) dispatch(clearEntries())
-					}}
-				>
-					Clear
-				</button>
-			</div>
-
-		</div >
+					<option hidden disabled selected>-- select an option --</option>
+					{workoutOptions}
+				</select>
+				<ul>
+					<ExerciseList />
+				</ul>
+				<div className='my-4'>
+					<button
+						className='px-5 rounded-full bg-green-700 mx-1 text-white  hover:bg-white hover:text-green-700 hover:outline'
+						onClick={() => dispatch(postExerciseEntries())}
+					>
+						Submit
+					</button>
+					<button
+						className='px-5 rounded-full bg-red-700 mx-1 text-white  hover:bg-white hover:text-red-700 hover:outline'
+						onClick={() => {
+							const a = confirm('Are you sure you want to clear the workout?')
+							if (a) dispatch(clearEntries())
+						}}
+					>
+						Clear
+					</button>
+				</div>
+			</main >
+		</Layout>
 	)
 }
 
