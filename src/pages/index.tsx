3046268aed129a68b05558c719e909a9460dc49a
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-03-01 12:51:40 -0700

    clean up the index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,79 +1,23 @@
-import React, { useEffect } from 'react'
 import { NextPage } from 'next'
 
-import { useAppDispatch, useAppSelector } from '@app/hooks'
-import Loading from '@features/loading/Loading'
-import ExerciseList from '@features/exercise/ExerciseList'
 import Layout from '@features/layout/Layout'
-import {
-	//actions
-	clearEntries,
-	//async actions
-	getWorkoutAsync,
-	getExerciseAsync,
-	//state grabbers
-	selectWorkouts,
-	postExerciseEntries
-}
-	from '@features/exercise/exerciseSlice'
+import Link from 'next/link'
 
 
 const IndexPage: NextPage = () => {
-	const dispatch = useAppDispatch()
-	const workouts = useAppSelector(selectWorkouts)
-	const status = useAppSelector(state => state.exercise.status)
-
-	//grab the workout templates from the server on page load
-	useEffect(() => {
-		dispatch(getWorkoutAsync())
-	}, [])
-
-	//map the options that we have for a workout ie push heavy , legs light etc
-	const workoutOptions = workouts.map((item, idx) => {
-		return (
-			<option
-				data-workout-type={item.type}
-				value={item.id}
-				key={idx}
-			>
-				{item.type}
-			</option>
-		)
-	})
-
-
-	if (status === 'loading') return <Loading />
 	return (
 		<Layout>
 			<main className='text-center'>
-				<h1 className='text-white'>Exercise to do</h1>
-				<select
-					className="text-slate-700 px-4 rounded-full outline outline-green-700 outline-4 mb-4"
-					onChange={(e) => dispatch(getExerciseAsync(Number(e.target.value)))}
-				>
-					<option hidden disabled selected>-- select an option --</option>
-					{workoutOptions}
-				</select>
-				<ul>
-					<ExerciseList />
-				</ul>
-				<div className='my-4'>
-					<button
-						className='px-5 rounded-full bg-green-700 mx-1 text-white  hover:bg-white hover:text-green-700 hover:outline'
-						onClick={() => dispatch(postExerciseEntries())}
-					>
-						Submit
-					</button>
-					<button
-						className='px-5 rounded-full bg-red-700 mx-1 text-white  hover:bg-white hover:text-red-700 hover:outline'
-						onClick={() => {
-							const a = confirm('Are you sure you want to clear the workout?')
-							if (a) dispatch(clearEntries())
-						}}
-					>
-						Clear
-					</button>
+				<div className='mt-10'>
+					<h1 className='text-white mb-10 text-3xl'>Quick Links</h1>
+					<Link href='/workout'>
+						<a className={`rounded-full bg-cyan-500 text-white px-8 py-2 
+					 hover:outline hover:outline-cyan-500 hover:text-cyan-500 hover:bg-white`}>
+							Workouts
+						</a>
+					</Link>
 				</div>
+
 			</main >
 		</Layout>
 	)
