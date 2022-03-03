6c054ec3ee02943c62f7fdbc7708363610d1c580
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-03-03 11:48:59 -0700

    switch to create many to avoid half workouts from being submitted

--- src/server/postExerciseEntries.ts
+++ src/server/postExerciseEntries.ts
@@ -2,18 +2,18 @@ import prisma from 'prisma/prisma'
 
 export async function postExerciseEntries(entries: any, id:number): Promise<boolean> {
 	try {
-		await Promise.all(entries.map((entry: any) => {
-			return prisma.exerciseEntry.create({
-				data: {
+		await prisma.exerciseEntry.createMany({
+			data: entries.map((entry: any) => {
+				return ({
 					notes: entry.notes || undefined,
 					intensity: entry.intensity || undefined,
 					order: entry.order || undefined,
 					weights: entry.weights,
 					exerciseID: entry.id,
 					workoutID: id
-				}
+				})
 			})
-		}))
+		})
 		return true
 	} catch (error) {
 		console.error(error)
