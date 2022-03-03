ebe1748d82aae5c2efcdb8ed6c2a24d965c39d53
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-03-02 17:12:05 -0700

    clean up function

--- src/pages/exercise/[exerciseItem].tsx
+++ src/pages/exercise/[exerciseItem].tsx
@@ -1,11 +1,9 @@
 import React from 'react'
-// import Loading from '@features/loading/Loading'
 import Layout from '@features/layout/Layout'
 import Link from 'next/link'
 import { useAppDispatch, useAppSelector } from '@app/hooks'
 
-import { editWeight, editNotes, editIntensity, editOrder, selectActiveEntry, selectStatus } from '@features/exercise/exerciseSlice'
-import Loading from '@features/loading/Loading'
+import { editWeight, editNotes, editIntensity, editOrder, selectActiveEntry } from '@features/exercise/exerciseSlice'
 
 const ExerciseItem = () => {
 	const dispatch = useAppDispatch()
@@ -14,30 +12,48 @@ const ExerciseItem = () => {
 	console.log(`${activeExerciseId} : is the active ID`)
 	console.table(list)
 
-	//grab the active exercise item from the store
-	const item = useAppSelector(state => state.exercise.entries.find(elem => elem.id === activeExerciseId))
-	const status = useAppSelector(selectStatus)
-
-
-	if (status === 'loading') return <Loading />
+	//grab the active exercise exerciseEntry from the store
+	const exerciseEntry = useAppSelector(state => state.exercise.entries.find(elem => elem.id === activeExerciseId))
 
 	return (
 		<Layout>
 			<main>
-				{item ?
+				{exerciseEntry ?
 					<div>
 						<div className='grid grid-cols-4 my-6'>
-							<Link href={`/workout/${item.workoutId}`}><span className="flex items-center "><a className='text-center bg-red-700 px-8 rounded-full w-3/4 mx-auto text-white hover:outline-red-700 hover:outline hover:bg-white hover:text-red-700'>Back</a></span></Link>
-							<h2 className='mb-4 mx-auto1 text-white text-center col-span-2'><span className='underline text-3xl'>{item.name}</span> <br /> Sets {item.sets}x{item.reps}</h2>
+							<Link href={`/workout/${exerciseEntry.workoutId}`}>
+								<span className="flex item-center">
+									<a className='text-center bg-red-500 px-8 rounded-full w-3/4 mx-auto text-white hover:outline-red-500 hover:outline hover:bg-white hover:text-red-500'
+									>Back
+									</a>
+								</span>
+							</Link>
+							<h2 className='mb-4 mx-auto1 text-white text-center col-span-2'>
+								<span className='underline text-3xl'>
+									{exerciseEntry.name}
+								</span>
+								<br />
+								Sets {exerciseEntry.sets}x{exerciseEntry.reps}
+							</h2>
 						</div>
+						<input
+							type="number"
+							placeholder='Order'
+							className='outline my-1 outline-blue-700 outline-4 rounded-full placeholder:text-slate-600 text-center py-3 w-11/12 block mx-auto mb-10'
+							value={exerciseEntry.order}
+							onChange={(e) => dispatch(editOrder({
+								movementID: exerciseEntry.movementID,
+								value: Number(e.target.value)
+							}))}
+						/>
 						<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 content-center w-11/12 mx-auto">
-							{item.weights.map((weight, i: number) => (
+							{exerciseEntry.weights.map((weight, i: number) => (
 								<input
 									key={i}
 									type="number"
 									value={weight}
 									onChange={(e) => dispatch(editWeight({
-										movementID: item.movementID,
+										movementID: exerciseEntry.movementID,
 										value: Number(e.target.value),
 										setNumber: i
 									}))}
@@ -46,14 +62,14 @@ const ExerciseItem = () => {
 								/>
 							))}
 						</div>
-						<div className="mt-4 grid gap-7 grid-cols-1 md:grid-cols-3 content-center w-11/12 mx-auto">
+						<div className="mt-4 grid gap-7 grid-cols-1 md:grid-cols-2 content-center w-11/12 mx-auto">
 							<input
 								type="text"
 								placeholder='Notes'
 								className='outline my-1 outline-yellow-400 outline-4 rounded-full placeholder:text-slate-600 text-center py-3'
-								value={item.notes}
+								value={exerciseEntry.notes}
 								onChange={(e) => dispatch(editNotes({
-									movementID: item.movementID,
+									movementID: exerciseEntry.movementID,
 									value: e.target.value
 								}))}
 							/>
@@ -61,36 +77,26 @@ const ExerciseItem = () => {
 								type="number"
 								placeholder='Intensity 0-10'
 								className='outline my-1 outline-purple-600 outline-4 rounded-full placeholder:text-slate-600 text-center py-3'
-								value={item.intensity}
+								value={exerciseEntry.intensity}
 								min={0}
 								max={10}
 								onChange={(e) => dispatch(editIntensity({
-									movementID: item.movementID,
-									value: Number(e.target.value)
-								}))}
-							/>
-							<input
-								type="number"
-								placeholder='Order'
-								className='outline my-1 outline-blue-700 outline-4 rounded-full placeholder:text-slate-600 text-center py-3'
-								value={item.order}
-								onChange={(e) => dispatch(editOrder({
-									movementID: item.movementID,
+									movementID: exerciseEntry.movementID,
 									value: Number(e.target.value)
 								}))}
 							/>
 						</div>
 					</div>
 					:
-					<div>
-						<h1 className='text-white text-3xl text-center'>No item selected</h1>
+					(<div>
+						<h1 className='text-white text-3xl text-center'>No exerciseEntry selected</h1>
 						<Link href='/workout'>
 							<button className='bg-white text-cyan-500 rounded-full' >Back to Workouts</button>
 						</Link>
 					</div>
+					)
 				}
 			</main>
-
 		</Layout>
 	)
 }
