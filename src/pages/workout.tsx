db56e1ecd72b3dccecd92711f6293e083ed7cf2a
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-03-03 10:50:51 -0700

    pass selector

--- src/pages/workout.tsx
+++ src/pages/workout.tsx
@@ -5,12 +5,12 @@ import Loading from '@features/loading/Loading'
 import Layout from '@features/layout/Layout'
 
 import { useAppDispatch, useAppSelector } from '@app/hooks'
-import { getWorkoutAsync, selectWorkouts, getExerciseTemplates } from '@features/exercise/exerciseSlice'
+import { getWorkoutAsync, selectWorkouts, getExerciseTemplates, selectStatus } from '@features/exercise/exerciseSlice'
 
 const WorkoutPage: NextPage = () => {
 	const dispatch = useAppDispatch()
 	const workouts = useAppSelector(selectWorkouts)
-	const status = useAppSelector(state => state.exercise.status)
+	const status = useAppSelector(selectStatus)
 
 	//grab the workout templates from the server on page load
 	useEffect(() => {
@@ -31,7 +31,7 @@ const WorkoutPage: NextPage = () => {
 								bg-cyan-500 rounded-full my-12 py-4 hover:bg-white hover:outline outline-cyan-500 text-white
 								hover:text-cyan-500 hover:cursor-pointer w-11/12 mx-auto`
 								}
-								onClick={() => dispatch(getExerciseTemplates(item.id))}
+									onClick={() => dispatch(getExerciseTemplates(item.id))}
 								>
 									<a className="capitalize">{item.type}</a>
 									<p>Last Worked on : N/A</p>
