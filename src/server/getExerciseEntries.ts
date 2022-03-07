76a3713020cc4a4a5f3f4a32415810243debca70
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-03-07 14:17:31 -0700

    get the exercise entries given a workoutID

---
+++ src/server/getExerciseEntries.ts
@@ -0,0 +1,16 @@
+import prisma from 'prisma/prisma'
+import { ExerciseEntry } from '@prisma/client'
+
+//get a list of exercise entries given a workoutId
+export async function getExerciseEntries(id: number): Promise<ExerciseEntry[]> {
+	try {
+		const exerciseList = await prisma.exerciseEntry.findMany({
+			where:{workoutID: id},
+		})
+		return exerciseList
+	}
+	catch(err) {
+		console.error(err)
+		return null
+	}
+}
