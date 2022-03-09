55e086230d102b50ee511c1f5f4955ce631f8135
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-03-09 11:34:53 -0700

    add some color to exercies

--- src/features/exercise/ExerciseList.tsx
+++ src/features/exercise/ExerciseList.tsx
@@ -17,30 +17,34 @@ const ExerciseList = () => {
 	const dispatch = useAppDispatch()
 	const exercises = useAppSelector(selectEntries)
 	return (
-		<ul className='grid grid-cols-2 md:grid-cols-3 bg-slate-700 w-11/12 mx-auto my-4 rounded-2xl'>
-			{exercises.map((item, idx) => {
-				return (
-					<Link key={idx} href={`/exercise/${item.id}`}>
-						<li
-							className={
-								`w-11/12 text-white my-6 py-4 rounded-3xl mx-auto hover:cursor-pointer hover:bg-white hover:outline 
+		<div className='bg-slate-700  w-11/12 mx-auto rounded-2xl'>
+			<h2 className='text-white text-xl'>Exercises</h2>
+			<ul className='grid grid-cols-2 md:grid-cols-3 mb-4'>
+				{exercises.map((item, idx) => {
+					return (
+						<Link key={idx} href={`/exercise/${item.id}`}>
+							<li
+								className={
+									`w-11/12 text-white my-6 py-4 rounded-3xl mx-auto shadow-lg shadow-black/70 hover:cursor-pointer hover:bg-white hover:outline 
 								${item.completed
-									? 'bg-purple-600  hover:text-purple-600 hover:outline-purple-600'
-									: 'bg-cyan-500  hover:text-cyan-500 hover:outline-cyan-500'
-								}  `
-							}
-							onClick={() => { dispatch(setActiveEntry(item.id)) }}
-						>
-							<span >
-								<strong className='text-l'>{item.name}</strong>
-								<br /> {item.sets} sets x {item.reps} reps
-								<br /> order: {item.order ? item.order : 'N/A'}
-							</span>
-						</li>
-					</Link>
-				)
-			})}
-		</ul>
+										? 'bg-purple-600  hover:text-purple-600 hover:outline-purple-600'
+										: 'bg-cyan-500  hover:text-cyan-500 hover:outline-cyan-500'
+									}  `
+								}
+								onClick={() => { dispatch(setActiveEntry(item.id)) }}
+							>
+								<span >
+									<strong className='text-l'>{item.name}</strong>
+									<br /> {item.sets} sets x {item.reps} reps
+									<br /> order: {item.order ? item.order : 'N/A'}
+								</span>
+							</li>
+						</Link>
+					)
+				})}
+			</ul>
+		</div>
+
 	)
 }
 
--- src/pages/workout/[exerciseTemplate].tsx
+++ src/pages/workout/[exerciseTemplate].tsx
@@ -7,20 +7,24 @@ import { postExerciseEntries } from '@features/exercise/exerciseSlice'
 import Loading from '@features/loading/Loading'
 const ExerciseTemplate = () => {
 	const status = useAppSelector(state => state.exercise.status)
+	const workoutID = useAppSelector(state => state.exercise.activeWorkout)
+
+	const activeWorkout = useAppSelector(state => state.exercise.workouts.find(w => w.id === workoutID))
 	const dispatch = useAppDispatch()
 
 	if (status === 'loading') return <Loading />
 	return (
 		<Layout>
 			<main className='text-center mt-4'>
-				<div className='grid grid-cols-4'>
+				<div className='grid grid-cols-4 mb-4'>
 					<Link href='/workout'><a className='text-white bg-red-500 rounded-full w-11/12 mx-auto hover:bg-white hover:text-red-500 hover:outline outline-red-500'>Back</a></Link>
-					<h1 className='text-white col-span-2 mx-auto underline text-3xl'>Exercises to Complete</h1>
+					<h1 className='text-white col-span-2 mx-auto underline text-3xl capitalize'>{activeWorkout && activeWorkout.type}</h1>
 				</div>
 				<ExerciseList />
-				<div className="w-11/12 mx-auto ">
-					<textarea className="w-full rounded mb-10"
-						placeholder="Add notes here"
+				<div className="w-11/12 mx-auto bg-slate-700 rounded-3xl">
+					<h2 className='text-white text-xl'>Notes</h2>
+					<textarea className="w-11/12 rounded my-4 h-24 outline outline-yellow-500"
+						placeholder="Include any notes about the workout in general"
 					></textarea>
 					<button className='text-white bg-green-500 rounded-full w-11/12 mx-auto hover:bg-white hover:text-green-500 hover:outline outline-green-500 mb-4 h-16'
 						onClick={() => dispatch(postExerciseEntries())}>
