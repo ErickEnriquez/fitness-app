5a56c4d421ac425b98a469861de592cb5db2131b
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-03-09 11:39:29 -0700

    add drop shadow

--- src/features/exercise/ExerciseList.tsx
+++ src/features/exercise/ExerciseList.tsx
@@ -25,7 +25,8 @@ const ExerciseList = () => {
 						<Link key={idx} href={`/exercise/${item.id}`}>
 							<li
 								className={
-									`w-11/12 text-white my-6 py-4 rounded-3xl mx-auto shadow-lg shadow-black/70 hover:cursor-pointer hover:bg-white hover:outline 
+									`w-11/12 text-white my-6 py-4 rounded-3xl mx-auto shadow-lg shadow-black/70 
+									hover:cursor-pointer hover:bg-white hover:outline 
 								${item.completed
 										? 'bg-purple-600  hover:text-purple-600 hover:outline-purple-600'
 										: 'bg-cyan-500  hover:text-cyan-500 hover:outline-cyan-500'
@@ -36,7 +37,7 @@ const ExerciseList = () => {
 								<span >
 									<strong className='text-l'>{item.name}</strong>
 									<br /> {item.sets} sets x {item.reps} reps
-									<br /> order: {item.order ? item.order : 'N/A'}
+									<br /> Order: {item.order ? item.order : 'N/A'}
 								</span>
 							</li>
 						</Link>
