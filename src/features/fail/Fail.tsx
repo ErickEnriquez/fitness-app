eec260b2353cc5a3f45d10a2f99c40a74cf8f062
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-04-01 13:39:41 -0700

    add a button to the failed component to exit

--- src/features/fail/Fail.tsx
+++ src/features/fail/Fail.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 
-const Fail = () => {
+interface props {
+	clickHandler?: () => void
+}
+
+const Fail = ({ clickHandler }: props) => {
 	return (
 		<div role="alert">
-			<div className="bg-red-500 text-white font-bold rounded-t px-4 py-2">
-				Error
+			<div className="bg-red-500 text-white font-bold rounded-t px-4 py-2 flex justify-between">
+				<span>Error</span>
+				<button
+					onClick={clickHandler}
+					className='bg-cyan-500 px-5 rounded-full shadow-lg shadow-black/50'>Exit
+				</button>
+
+
 			</div>
 			<div className="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
 				<p>Something went wrong, please try again later</p>
