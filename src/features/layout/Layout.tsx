7ba56cd3e948c2e6d1d9d4d7f27c0efb27db9daa
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-04-01 15:17:14 -0700

    include padding to card

--- src/features/layout/Layout.tsx
+++ src/features/layout/Layout.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../../styles/Home.module.css'
+
 interface LayoutProps {
 	title?: string
 	children?: React.ReactNode
