dd672683b49bcc5fdc6c25251f751e1e2e6c901d
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-05-17 11:59:40 -0700

    add pointer to cursor on hover

--- src/features/calendar/calendarHeader.tsx
+++ src/features/calendar/calendarHeader.tsx
@@ -10,20 +10,23 @@ interface props {
 
 const CalendarHeader = ({ activeDate, setActiveDate, setSelectedDate }: props) => {
 	return (
-		<div className="text-white w-11/12 mx-auto flex justify-evenly	mt-4">
-			<h3 onClick={() => {
-				setSelectedDate(new Date())
-				setActiveDate(new Date())
-			}}
+		<div className="text-white w-11/12 mx-auto flex justify-evenly	mt-4 bg-slate-500 py-2 rounded-xl">
+			<h3
+				className='hover:cursor-pointer'
+				onClick={() => {
+					setSelectedDate(new Date())
+					setActiveDate(new Date())
+				}}
 			>Today
 			</h3>
 			<AiOutlineLeft
 				onClick={() => setActiveDate(subMonths(activeDate, 1))}
-				className="navIcon"
+				className="hover:cursor-pointer font-heavy"
 			/>
 			<AiOutlineRight
 				onClick={() => setActiveDate(addMonths(activeDate, 1))}
-				className="navIcon" />
+				className="hover:cursor-pointer font-heavy"
+			/>
 			<h2 className="currentMonth">{format(activeDate, 'MMMM yyyy')}</h2>
 		</div>
 	)
