0a05b10e958b62c53ff24ac76858c9b9062cc37e
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-05-23 14:07:18 -0700

    add a loading animation to the calendar

--- src/pages/calendar.tsx
+++ src/pages/calendar.tsx
@@ -1,8 +1,18 @@
-import Layout from '@features/layout/Layout'
-import CalendarSection from '@features/calendar/CalendarSection'
 import React from 'react'
 
+import Layout from '@features/layout/Layout'
+import CalendarSection from '@features/calendar/CalendarSection'
+import Loading from '@features/loading/Loading'
+
+import {  useAppSelector } from '@app/hooks'
+import { selectStatus } from '@features/calendar/CalendarSlice'
+
 const Calendar = () => {
+	const status = useAppSelector(selectStatus)
+	//grab the workout templates from the server on page load
+
+	if (status === 'loading') return <Layout><Loading /></Layout>
+
 	return (
 		<Layout>
 			<CalendarSection />
