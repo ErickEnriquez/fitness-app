5692217b9b3b8406f4beeaa8203a2318f5e1065e
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-06-06 15:52:28 -0700

    clean up unused imports

--- src/features/calendar/CalendarSection.tsx
+++ src/features/calendar/CalendarSection.tsx
@@ -1,17 +1,10 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 
 import Layout from '@features/layout/Layout'
 import CalendarHeader from '@features/calendar/CalendarHeader'
 import CalendarWeekDays from '@features/calendar/CalendarWeekDays'
 import CalendarDates from '@features/calendar/CalendarDates'
 import Card from '@components/Card'
-
-import { useAppDispatch, useAppSelector } from '@app/hooks'
-
-import { getWorkoutsAsync, selectActiveDate, selectWorkouts } from '@features/calendar/CalendarSlice'
-
-import { startOfMonth, endOfMonth } from 'date-fns'
-
 
 const Calendar = () => {
 
