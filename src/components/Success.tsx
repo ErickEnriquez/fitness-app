a47b96576ac2368242c3750b1144f9e975af47c9
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-07-06 11:12:02 -0700

    styling changes for success component

--- src/components/Success.tsx
+++ src/components/Success.tsx
@@ -6,16 +6,16 @@ interface props {
 
 const Success = ({ clickHandler }: props) => {
 	return (
-		<div role="alert">
-			<div className="bg-green-100 border-green-500 text-white font-bold rounded-t px-4 py-2 flex justify-between">
-				<span>Error</span>
+		<div role="alert" className="mx-auto w-11/12 pt-8">
+			<div className="bg-green-600 border-green-500 text-white font-bold rounded-t px-4 py-2 flex justify-between">
+				<h2 className="mx-auto text-xl">Success</h2>
 				<button
 					onClick={clickHandler}
 					className='bg-cyan-500 px-5 rounded-full shadow-lg shadow-black/50'>Exit
 				</button>
 			</div>
-			<div className="flex justify-left">
-				<div>
+			<div className="flex justify-left bg-green-300 py-4">
+				<div className='mx-auto'>
 					<p className="font-bold">Workout has been successfully uploaded</p>
 					<p className="text-sm">You can close this tab safely</p>
 				</div>
