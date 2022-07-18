30ca68d739efd6e5a1933b55dc360f0798125fef
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-07-18 14:52:33 -0700

    make the title conditionally render

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,9 +9,10 @@ interface props {
 const Card = ({ title, children, style }: props) => {
 	return (
 		<div className="bg-slate-700 rounded-3xl content-center w-11/12 mx-auto py-4" >
-			<h2
+			{title && <h2
 				style={style}
-				className='text-2xl font-bold text-white'>{title || 'PlaceholderTitle'}</h2>
+				className='text-2xl font-bold text-white'>{title}</h2>
+			}
 			{children}
 		</div>
 	)
