06557bdb1a98cad3f4eb9e9ba3429c53c9f26cf7
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-07-25 14:04:33 -0700

    clearn up endpoint

--- src/pages/api/workout-entry.ts
+++ src/pages/api/workout-entry.ts
@@ -1,19 +1,45 @@
+/* eslint-disable indent */
 import { NextApiRequest, NextApiResponse } from 'next'
-import { getLastWorkout } from '@server/getLastWorkout' 
+import { getLastWorkoutOfType } from '@server/getLastWorkoutOfType'
+import { getWorkoutEntry} from '@server/getWorkoutEntry'
 import { Workout } from '@prisma/client'
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-	
-	if (req.method !== 'POST') {
-		res.status(405).json({ message: 'Method not allowed' })
-	}
 
+	switch (req.method) {
+		case 'GET': await getPreviousWorkout(req, res)
+			break
+		case 'POST': await lastWorkoutOfType(req, res)
+			break
+		default:
+			res.status(405).json({ message: 'Method not allowed' })
+	}
+}
+
+/**
+ * get the last workout given a workoutID
+ * @param req 
+ * @param res 
+ */
+const getPreviousWorkout = async (req: NextApiRequest, res: NextApiResponse) => {
+	const workoutID = Number(req.query.workoutID)
+	const workout = await getWorkoutEntry(workoutID)
+	return res.status(200).json(workout)
+}
+
+/**
+ * return the last workout of the given type ie. pusheavy, pullheavy, etc.
+ * @param req 
+ * @param res 
+ * @returns 
+ */
+const lastWorkoutOfType = async (req: NextApiRequest, res: NextApiResponse) => {
 	const workoutTemplates = req.body.workoutTemplates.data as Workout[]
-	
+
 	const lastWorkouts = await (
 		await Promise.all(workoutTemplates
-			.map((item) => getLastWorkout(item.id))))
+			.map((item) => getLastWorkoutOfType(item.id))))
 		.filter(item => item !== null)
-	
+
 
 	return res.status(200).json(lastWorkouts)
 }
