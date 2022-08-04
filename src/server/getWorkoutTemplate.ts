ccbc7d5c57552f403fcd87ce93f7e92a617495b6
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-08-04 15:41:04 -0700

    getWorkoutTemplate now only returns workouts for a given program

--- src/server/getWorkoutTemplate.ts
+++ src/server/getWorkoutTemplate.ts
@@ -1,7 +1,22 @@
 import prisma from 'prisma/prisma'
-import {Workout} from '@prisma/client'
-//return all of the possible workout types , ie push heavy, pull heavy, legs light
-export async function getWorkoutTemplate():Promise<Workout[]> {
-	const workoutArray = await prisma.workout.findMany({})
-	return workoutArray
+import { Workout } from '@prisma/client'
+
+/**
+ * Given a programID return all of the workouts that belong to that workout
+ * @param programId 
+ * @returns 
+ */
+export async function getWorkoutTemplate(programId: number): Promise<Workout[]> {
+	try {
+		const workoutArray = await prisma.workout.findMany({
+			where: {
+				programID: programId
+			}
+		})
+		return workoutArray
+	}
+	catch (err) {
+		console.error(err)
+		throw Error('Unable to get workout templates')
+	}
 }
