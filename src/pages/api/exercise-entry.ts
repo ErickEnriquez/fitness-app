c45f33fb75963716e2044e4efe09ceff1cccd09b
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-08-04 16:24:41 -0700

    add auth to exercise entry

--- src/pages/api/exercise-entry.ts
+++ src/pages/api/exercise-entry.ts
@@ -1,17 +1,28 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { postExerciseEntries } from '@server/postExerciseEntries'
-import {createWorkoutEntry} from '@server/createWorkoutEntry'
-import {getExerciseEntries} from '@server/getExerciseEntries'
+import { createWorkoutEntry } from '@server/createWorkoutEntry'
+import { getExerciseEntries } from '@server/getExerciseEntries'
 import { activeWorkoutInfo } from '@features/exercise/exerciseSlice'
-export default async (req: NextApiRequest, res: NextApiResponse) => { 
 
-	switch (req.method) { 
-	case 'POST':
-		await postEntries(req, res); break
-	case 'GET':
-		await getWorkoutEntries(req, res); break
-	default:
-		res.status(405).json({ message: 'Method not allowed' })
+import { unstable_getServerSession } from 'next-auth/next'
+import { authOptions } from '@auth/[...nextauth]'
+
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+	const session = await unstable_getServerSession(req, res, authOptions)
+
+	if (!session) {
+		res.status(401).json({ message: 'unauthorized' })
+		return
+	}
+
+	switch (req.method) {
+		case 'POST':
+			await postEntries(req, res); break
+		case 'GET':
+			await getWorkoutEntries(req, res); break
+		default:
+			res.status(405).json({ message: 'Method not allowed' })
 	}
 }
 
@@ -19,16 +30,16 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 async function postEntries(req: NextApiRequest, res: NextApiResponse) {
 	//grab the data, which should be an array of user entries and a workoutId
 	const data = req.body
-	
-	const activeWorkout = data.workoutEntry as activeWorkoutInfo 
+
+	const activeWorkout = data.workoutEntry as activeWorkoutInfo
 	//create the workout entry in the db first
 	const workoutEntry = await createWorkoutEntry(activeWorkout)
-	if (!workoutEntry) { 
+	if (!workoutEntry) {
 		res.status(500).json({ message: 'Failed to create workout' })
 	}
 	//then create the exercise entries in the db and link them to a workout
 	const response = await postExerciseEntries(data.entries, workoutEntry as number)
-	
+
 	response ?
 		res.status(200).json({ message: 'Success' })
 		: res.status(500).json({ message: 'Error' })
@@ -41,5 +52,5 @@ async function getWorkoutEntries(req: NextApiRequest, res: NextApiResponse) {
 	const data = await getExerciseEntries(id)
 	data ?
 		res.status(200).json(data) :
-		res.status(200).json({data:null})
+		res.status(200).json({ data: null })
 }
