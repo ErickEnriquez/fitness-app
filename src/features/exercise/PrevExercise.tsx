56389e2b91154d5b93e40bce27eac98987362e6d
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-08-04 20:42:13 -0700

    [WIP] add a more or less button to the previous workouts

--- src/features/exercise/PrevExercise.tsx
+++ src/features/exercise/PrevExercise.tsx
@@ -1,8 +1,16 @@
 import React, { useState } from 'react'
 import { useAppSelector, useAppDispatch } from '@app/hooks'
-import { selectPreviousExerciseEntries, selectActiveEntry, getMorePreviousWorkouts } from '@features/exercise/exerciseSlice'
+import {
+	selectPreviousExerciseEntries,
+	selectActiveEntry,
+	getMorePreviousWorkouts,
+	removePreviousWorkout
+} from '@features/exercise/exerciseSlice'
+
 import { format } from 'date-fns'
+
 import Card from '@components/Card'
+import Button from '@components/util/Button'
 
 const PrevExercise = () => {
 	//get the list of all of the exercises of the previous workout
@@ -34,13 +42,11 @@ const PrevExercise = () => {
 							<tr>
 								<td>{activePrevExercise.order}</td>
 								<td>{activePrevExercise.intensity}</td>
-								{/* placeholder date value right now, replace with the actual date that the workout was completed  */}
-								<td>{format(new Date(workout.date), 'MMM  dd yyyy')}</td>
+								<td>{format(new Date(workout.date), 'MM/dd/yyyy')}</td>
 							</tr>
 						</tbody>
 					</table>
 					<div className=' w-11/12 mx-auto mt-4'>
-						<strong className='text-xl'>Weight</strong>
 						<table className='w-full mt-4'>
 							<thead>
 								<tr className='bg-cyan-700 border-collapse rounded-2xl'>
@@ -68,17 +74,32 @@ const PrevExercise = () => {
 			)
 		})
 		: null
-	
+
 
 	return (
 		<div className='text-white text-center my-4'>
 			{previousWorkouts ?
 				<Card title='Previous Workouts'>
 					{prevWorkoutsList}
-					<button onClick={() => {
-						dispatch(getMorePreviousWorkouts(skipAmount))
-						setSkipAmount(skipAmount + 1)
-					}}> Load More</button>
+					<div className='grid grid-cols-2 w-11/12 mx-auto mt-4'>
+						<Button
+							clickHandler={() => {
+								dispatch(getMorePreviousWorkouts(skipAmount))
+								setSkipAmount(skipAmount + 1)
+							}}
+							text='More'
+						/>
+						<Button
+							clickHandler={() => {
+								dispatch(removePreviousWorkout())
+								if (skipAmount >= 0) {
+									return
+								}
+								setSkipAmount(skipAmount + 1)
+							}}
+							text='Less'
+						/>
+					</div>
 				</Card>
 				: (
 					<h3 className='text-2xl'>
