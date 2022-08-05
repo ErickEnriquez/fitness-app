96fef219ef611b93478d81e23f827109ced7ba3e
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-08-04 20:18:54 -0700

    pass userId to create new cardio endpoint

--- src/server/postCardio.ts
+++ src/server/postCardio.ts
@@ -2,7 +2,7 @@ import prisma from 'prisma/prisma'
 import { CardioState } from '@features/cardio/CardioSlice'
 import { CardioType } from '@prisma/client'
 
-export async function postCardio(val: CardioState): Promise<boolean> {
+export async function postCardio(val: CardioState, userID = ''): Promise<boolean> {
 	try {
 		await prisma.cardio.create({
 			data: {
@@ -11,7 +11,8 @@ export async function postCardio(val: CardioState): Promise<boolean> {
 				distance: val.distance,
 				time: val.time,
 				notes: val.notes,
-				type: CardioType[val.cardioType]
+				type: CardioType[val.cardioType],
+				userId: userID,
 			}
 		})
 		return true
