d0c29fd2b6c7161b79658d12969ecf2c26f06b7e
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-09-03 14:15:26 -0700

    grab the name of the workout as well as the reps

--- src/features/history/PreviousWorkoutSlice.ts
+++ src/features/history/PreviousWorkoutSlice.ts
@@ -3,19 +3,26 @@ import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
 import axios from 'axios'
 
 import type { AppState } from '@app/store'
-import { WorkoutEntry, ExerciseEntry } from '@prisma/client'
+import { WorkoutEntry, ExerciseEntry, ExerciseTemplate, Movement } from '@prisma/client'
+
+//expand the ExerciseEntry field to include the name of the exercise as well as the number of sets
+export interface PreviousExercise extends ExerciseEntry {
+	name: string,
+	reps: number
+}
+
 
 export interface PreviousWorkoutState {
 	status: 'idle' | 'loading' | 'failed' | 'success'
 	workout: WorkoutEntry
-	exercises: ExerciseEntry[]
+	exercises: PreviousExercise[]
 }
 
 
 const initialState = {
 	status: 'idle',
 	workout: null as WorkoutEntry,
-	exercises: [] as ExerciseEntry[]
+	exercises: [] as PreviousExercise[]
 }
 
 /**
@@ -35,7 +42,17 @@ export const getWorkoutDataAsync = createAsyncThunk(
 			const exercises = response[1].data as ExerciseEntry[]
 			const prevWorkout = response[0].data as WorkoutEntry
 
-			return { exercises, workout: prevWorkout }
+			const templates = await Promise.all(exercises
+				.map(exercise => axios.get('/api/exerciseEntry', { params: { exerciseId: exercise.exerciseID } }))
+			).then(r => r.map(t => t.data)) as ExerciseTemplate[]
+
+			const movements = await Promise.all(templates.map(template => {
+				return axios.get('/api/movement', { params: { movementId: template.movementID } })
+			})).then(r => r.map(m => m.data)) as Movement[]
+
+			const data = exercises.map((e, idx) => ({ ...e, reps: templates[idx].reps, name: movements[idx].name })) as PreviousExercise[]
+
+			return { exercises: data, workout: prevWorkout }
 		} catch (err) {
 			console.error(err)
 			return rejectWithValue(err)
