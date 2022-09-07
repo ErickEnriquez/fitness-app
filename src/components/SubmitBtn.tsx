55e43f3504b94bfe083c1e412aed7ac22a42814f
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-09-06 18:47:34 -0700

    add option to disable submit button

--- src/components/SubmitBtn.tsx
+++ src/components/SubmitBtn.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 interface SubmitProps {
 	clickHandler: () => void
+	isDisabled?: boolean
 }
 
-const SubmitBtn = ({ clickHandler }: SubmitProps) => {
+const SubmitBtn = ({ clickHandler, isDisabled }: SubmitProps) => {
 	return (
 		<button
+			disabled={isDisabled}
 			type="submit"
-			className='text-white bg-green-600 rounded-full w-11/12 mx-auto hover:bg-white hover:text-green-600 hover:outline outline-green-600 my-4 h-12'
+			className={`my-4 h-12 text-white ${isDisabled && 'bg-green-600 outline-green-600 rounded-full w-11/12 mx-auto hover:bg-white hover:text-green-600 hover:outline'} `}
 			onClick={clickHandler}
 		>
 			Submit
