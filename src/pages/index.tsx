5620f16ba1f5b4fcddea4d5b8b388cdfcf7f6321
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-09-08 12:34:49 -0700

    convert old links to use new button component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,40 +2,32 @@ import { NextPage } from 'next'
 
 import Layout from '@components/Layout'
 import Link from 'next/link'
+import Card from '@components/Card'
+import Button from '@components/util/Button'
 
 const IndexPage: NextPage = () => {
 	return (
 		<Layout >
-			<main className='text-center'>
-				<div className='mt-10 flex flex-col'>
+			<main className='mt-10'>
+				<Card>
 					<h1 className='text-white mb-10 text-3xl'>{'Welcome'}</h1>
 					<h2 className='text-white mb-10 text-2xl'>Quick Links</h2>
-					<Link href='/workout'>
-						<a className={`rounded-full bg-primary-blue text-white px-8 py-2 mb-6 w-11/12 mx-auto
-					 hover:outline hover:outline-primary-blue hover:text-primary-blue hover:bg-white`}>
-							My workouts
-						</a>
-					</Link>
-					<Link href='/calendar'>
-						<a className={`rounded-full bg-primary-blue text-white px-8 py-2 mb-6 w-11/12 mx-auto
-					 hover:outline hover:outline-primary-blue hover:text-primary-blue hover:bg-white`}>
-							Workout calendar
-						</a>
-					</Link>
-					<Link href='/cardio'>
-						<a className={`rounded-full bg-primary-blue text-white px-8 py-2 mb-6 w-11/12 mx-auto
-					 hover:outline hover:outline-primary-blue hover:text-primary-blue hover:bg-white`}>
-							Cardio sessions
-						</a>
-					</Link>
-					<Link href='/'>
-						<a className={`rounded-full bg-primary-blue text-white px-8 py-2 mb-6 w-11/12 mx-auto
-					 hover:outline hover:outline-primary-blue hover:text-primary-blue hover:bg-white`}>
-							{'[WIP]'} Manage workouts
-						</a>
-					</Link>
-				</div>
-			</main >
+					<div className='grid grid-col gap-8 mb-4'>
+						<Link href='/workout'>
+							<a> <Button text="My Workouts" color='primary-blue' /> </a>
+						</Link>
+						<Link href='/calendar'>
+							<a> <Button text="Workout Calendar" color='primary-blue' /> </a>
+						</Link>
+						<Link href='/cardio'>
+							<a> <Button text="Cardio Sessions" color='primary-blue' /> </a>
+						</Link>
+						<Link href='/'>
+							<a ><Button text="WIP manage workouts" color='primary-blue' /></a>
+						</Link>
+					</div>
+				</Card>
+			</main>
 		</Layout>
 	)
 }
