f3b2a858114efb37ff1d39c74a64bf09e0672644
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-09-07 19:08:19 -0700

    convert more colors to use defined colors

--- src/components/BackBtn.tsx
+++ src/components/BackBtn.tsx
@@ -6,8 +6,8 @@ const BackBtn = ({ href, clickHandler }: { href: string, clickHandler?: () => vo
 			<span className="flex items-center">
 				<a onClick={clickHandler}
 					className={`
-									text-center bg-purple-700 px-8 rounded-full w-3/4 mx-auto text-white shadow-lg shadow-black/70 
-									hover:outline-purple-700 hover:outline hover:bg-white hover:text-purple-700
+									text-center bg-primary-purple px-8 rounded-full w-3/4 mx-auto text-white shadow-lg shadow-black/70 
+									hover:outline-primary-purple hover:outline hover:bg-white hover:text-primary-purple
 									flex items-center justify-center h-10 font-bold
 									`}
 				>Back
