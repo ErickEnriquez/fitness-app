699a87b66f520a77ffeb237078c1cc934a7eafe6
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-09-08 19:22:08 -0700

    center the loading component on the screen

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,13 +2,9 @@
 import React from 'react'
 import ReactLoading from 'react-loading'
 const Loading = (): JSX.Element => {
-	const styles = {
-		marginTop: '50px',
-		display: 'flex',
-		justifyContent: 'center',
-	}
+
 	return (
-		<div style={styles}>
+		<div className='flex h-screen items-center justify-center'>
 			<ReactLoading type='spin' color='#06b6d4' height={100} width={100} />
 		</div>
 	)
