bf0ed6db505f89c1d90be48f9ebd19581a0f2621
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-10-05 22:25:42 -0700

    create simple intermediate loader for files

---
+++ src/components/index.ts
@@ -0,0 +1,25 @@
+import Card from './Card'
+import Fail from './Fail'
+import Layout from './Layout'
+import Loading from './Loading'
+import MobileMenu from './MobileMenu'
+import Nav from './Nav'
+import NavLinks from './NavLinks'
+import Notes from './Notes'
+import NumberInput from './NumberInput'
+import SignIn from './SignIn'
+import Success from './Success'
+
+export {
+	Card,
+	Fail,
+	Layout,
+	Loading,
+	MobileMenu,
+	Nav,
+	NavLinks,
+	Notes,
+	NumberInput,
+	SignIn,
+	Success
+}
