c9ad871a8a80f18cf6dde0efc180101a2d02f037
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-10-19 14:36:28 -0700

    move getWorkoutEntry into its folder

--- src/server/WorkoutEntry/getWorkoutEntry.ts
+++ src/server/WorkoutEntry/getWorkoutEntry.ts
@@ -1,7 +1,7 @@
 import { ExerciseEntry, WorkoutEntry } from '@prisma/client'
 import prisma from 'prisma/prisma'
 
-import { LastWorkoutEntry } from './getLastWorkoutOfType'
+import { LastWorkoutEntry } from '../getLastWorkoutOfType'
 /**
  * return the
  * @param ID this is either the id of the workout or the workout type that we will use to get the last workout of that type
@@ -9,9 +9,7 @@ import { LastWorkoutEntry } from './getLastWorkoutOfType'
  */
 export async function getWorkoutEntry(ID: number, skip?: number, workoutType = false): Promise<LastWorkoutEntry | LastWorkoutEntry & { exercises: ExerciseEntry[] }> {
 	try {
-		const workout = workoutType ?
-			await getWorkoutUsingType(ID, skip) :
-			await getWorkoutUsingID(ID)
+		const workout = await getWorkoutUsingID(ID)
 
 		return workout ?
 			{ ...workout, date: workout.date.toISOString() }
@@ -23,31 +21,13 @@ export async function getWorkoutEntry(ID: number, skip?: number, workoutType = f
 	}
 }
 
-/**
- * if we only have a workout type to use then go though all of the workout types , sort, then skip x numbers to get the last workout
- * @param workoutType 
- * @param skip 
- */
-const getWorkoutUsingType = async (workoutType: number, skipAmount?: number): Promise<WorkoutEntry & { exercises: ExerciseEntry[] }> => {
-	const workout = await prisma.workoutEntry.findFirst({
-		where: {
-			workoutTemplateId: workoutType
-		},
-		orderBy: { date: 'desc' },
-		skip: skipAmount,
-		include: {
-			exercises: true
-		}
-	})
-	return workout
-}
 
 /**
  * if we already know the workout Id that we want to get just get the data and return it
  * @param workoutID the id of the workout we want to get
  * @returns 
  */
-const getWorkoutUsingID = async (workoutID: number): Promise<WorkoutEntry> => {
+export const getWorkoutUsingID = async (workoutID: number): Promise<WorkoutEntry> => {
 	const workout = await prisma.workoutEntry.findFirst({
 		where: {
 			id: workoutID
