e3d7cf568f1eac9b288ab59e3ddefb0cc404c552
Erick Enriquez <erick.enriquez10.95@gmail.com>
2022-11-23 12:56:37 -0700

    include ExerciseEntries when getting workout entries by type

--- src/server/WorkoutEntry/getWorkoutEntryByType.ts
+++ src/server/WorkoutEntry/getWorkoutEntryByType.ts
@@ -1,18 +1,21 @@
 import prisma from 'prisma/prisma'
 
-import { SerializedWorkoutEntry } from './workoutEntry'
+import { WorkoutEntryWithExercises } from './workoutEntry'
 /**
  * if we only have a workout type to use then go though all of the workout types , sort, then skip x numbers to get the last workout
  * @param workoutType 
  * @param skipAmount 
  */
-export const getWorkoutEntryByType = async (workoutType: number, skipAmount?: number): Promise<SerializedWorkoutEntry> => {
+export const getWorkoutEntryByType = async (workoutType: number, skipAmount?: number): Promise<WorkoutEntryWithExercises> => {
 	const workout = await prisma.workoutEntry.findFirst({
 		where: {
 			workoutTemplateId: workoutType
 		},
 		orderBy: { date: 'desc' },
-		skip: skipAmount
+		skip: skipAmount,
+		include: {
+			exercises:true
+		}
 	})
 	if (!workout) throw Error('no workout entry data found')
 
