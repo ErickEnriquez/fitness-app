4b2f6c1e615d2121d641372c17264087a2c6ab5a
Erick Enriquez <erick.enriquez10.95@gmail.com>
2023-01-06 13:54:27 -0700

    change exerciseItem component to grab the item name

--- src/pages/exercise/[exerciseItem].tsx
+++ src/pages/exercise/[exerciseItem].tsx
@@ -47,7 +47,7 @@ const ExerciseItem = () => {
 							</Link>
 							<h2 className='mx-auto1 text-white text-center col-span-3 bg-dark-gray rounded-2xl'>
 								<span className='text-3xl font-bold'>
-									{exerciseEntry.movement.name}
+									{exerciseEntry.name}
 								</span>
 								<br />
 								Sets {exerciseEntry.sets}x{exerciseEntry.reps}
